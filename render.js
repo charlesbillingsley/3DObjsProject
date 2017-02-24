b8153bd2eae9b95db81a852f1d48c017be0c24d3
Charles Billingsley <charlesebillingsley@yahoo.com>
2017-02-23 19:56:38 -0500

    First commit
    The outline for our project. Needs the 3D shapes js files

---
+++ render.js
@@ -0,0 +1,68 @@
+/**
+ * Created by Hans Dulimarta.
+ */
+let modelMat = mat4.create();
+let canvas, paramGroup;
+let posAttr, colAttr, modelUnif;
+let gl;
+let obj;
+
+function main() {
+  canvas = document.getElementById("gl-canvas");
+
+  /* setup window resize listener */
+  window.addEventListener('resize', resizeWindow);
+
+  gl = WebGLUtils.create3DContext(canvas, null);
+  ShaderUtils.loadFromFile(gl, "vshader.glsl", "fshader.glsl")
+  .then (prog => {
+
+    /* put all one-time initialization logic here */
+    gl.useProgram (prog);
+    gl.clearColor (0, 0, 0, 1);
+    gl.enable(gl.CULL_FACE);
+    gl.enable(gl.DEPTH_TEST);
+    gl.cullFace(gl.BACK);
+
+    /* the vertex shader defines TWO attribute vars and ONE uniform var */
+    posAttr = gl.getAttribLocation (prog, "vertexPos");
+    colAttr = gl.getAttribLocation (prog, "vertexCol");
+    modelUnif = gl.getUniformLocation (prog, "modelCF");
+    gl.enableVertexAttribArray (posAttr);
+    gl.enableVertexAttribArray (colAttr);
+
+    /* calculate viewport */
+    resizeWindow();
+
+    /* initiate the render loop */
+    render();
+  });
+}
+
+function drawScene() {
+  gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+
+  if (obj) {
+    obj.draw(posAttr, colAttr, modelUnif, modelMat);
+  }
+}
+
+function render() {
+  drawScene();
+  requestAnimationFrame(render);
+}
+
+function createObject() {
+  obj = null;
+  mat4.identity(modelMat);
+}
+
+function resizeWindow() {
+  let w = 0.98 * window.innerWidth;
+  let h = 0.6 * window.innerHeight;
+  let size = Math.min(0.98 * window.innerWidth, 0.65 * window.innerHeight);
+  /* keep a square viewport */
+  canvas.width = size;
+  canvas.height = size;
+  gl.viewport(0, 0, size, size);
+}
