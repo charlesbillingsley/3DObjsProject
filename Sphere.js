9189959fde4b01cdf305548eb1686e9b318dcfa3
christopherfracassi <fracassc@mail.gvsu.edu>
2017-02-25 18:23:39 -0500

    adding sphere

---
+++ Sphere.js
@@ -0,0 +1,117 @@
+/**
+ * Created by christopherfracassi on 2/19/17.
+ */
+
+class Sphere {
+    /**
+     * Create a 3D cone with tip at the Z+ axis and base on the XY plane
+     * @param {Object} gl      the current WebGL context
+     * @param {Number} radius  radius of the cone base
+     * @param {Number} height  height of the cone
+     * @param {Number} subDiv  number of radial subdivision of the cone base
+     * @param {vec3}   col1    color #1 to use
+     * @param {vec3}   col2    color #2 to use
+     */
+    constructor (gl, radius, stacks, col1, col2) {
+
+        /* if colors are undefined, generate random colors */
+        if (typeof col1 === "undefined") col1 = vec3.fromValues(Math.random(), Math.random(), Math.random());
+        if (typeof col2 === "undefined") col2 = vec3.fromValues(Math.random(), Math.random(), Math.random());
+        let randColor = vec3.create();
+        let vertices = [];
+        this.vbuff = gl.createBuffer();
+
+        /* Instead of allocating two separate JS arrays (one for position and one for color),
+         in the following loop we pack both position and color
+         so each tuple (x,y,z,r,g,b) describes the properties of a vertex
+         */
+
+        for (let k = 0; k <= stacks; k++) {
+            let angle = k * Math.PI / stacks;
+            let sin = Math.sin(angle);
+            let cos =  Math.cos(angle);
+
+            for(let i = 0; i <= stacks; i++){
+                let angle2 = i * 2 * Math.PI / stacks;
+                let sin2 = Math.sin(angle2);
+                let cos2 = Math.cos(angle2);
+
+                let x = cos2 * sin;
+                let y = cos;
+                let z = sin2 * sin;
+                vertices.push(radius * x, radius * y, radius * z);
+                vec3.lerp (randColor, col1, col2, Math.random()); /* linear interpolation between two colors */
+                vertices.push(randColor[0], randColor[1], randColor[2]);
+
+            }
+        }
+
+        /* copy the (x,y,z,r,g,b) sixtuplet into GPU buffer */
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vbuff);
+        gl.bufferData(gl.ARRAY_BUFFER, Float32Array.from(vertices), gl.STATIC_DRAW);
+
+        // Generate index order for top of sphere
+        let topIndex = [];
+        for (let k = 0; k < stacks; k++){
+            for(let i = 0; i < stacks; i++){
+                let first = (k * (stacks + 1)) + i;
+                let second = first + stacks + 1;
+
+                topIndex.push(first);
+                topIndex.push(second);
+                topIndex.push(first + 1);
+
+                topIndex.push(second);
+                topIndex.push(second + 1);
+                topIndex.push(first + 1);
+
+
+            }
+        }
+        this.topIdxBuff = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.topIdxBuff);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint8Array.from(topIndex), gl.STATIC_DRAW);
+
+        // Generate index order for bottom of cone
+       /* let botIndex = [];
+        botIndex.push(subDiv + 1);
+        for (let k = subDiv; k >= 1; k--)
+            botIndex.push(k);
+        botIndex.push(subDiv);
+        this.botIdxBuff = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.botIdxBuff);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint8Array.from(botIndex), gl.STATIC_DRAW);
+        */
+
+
+
+            this.indices = [{"primitive": gl.TRIANGLE_FAN, "buffer": this.topIdxBuff, "numPoints": topIndex.length}];
+                //{"primitive": gl.TRIANGLE_FAN, "buffer": this.botIdxBuff, "numPoints": botIndex.length}];
+    }
+
+    /**
+     * Draw the object
+     * @param {Number} vertexAttr a handle to a vec3 attribute in the vertex shader for vertex xyz-position
+     * @param {Number} colorAttr  a handle to a vec3 attribute in the vertex shader for vertex rgb-color
+     * @param {Number} modelUniform a handle to a mat4 uniform in the shader for the coordinate frame of the model
+     * @param {mat4} coordFrame a JS mat4 variable that holds the actual coordinate frame of the object
+     */
+    draw(vertexAttr, colorAttr, modelUniform, coordFrame) {
+        /* copy the coordinate frame matrix to the uniform memory in shader */
+        gl.uniformMatrix4fv(modelUniform, false, coordFrame);
+
+        /* binder the (vertex+color) buffer */
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vbuff);
+
+        /* with the "packed layout"  (x,y,z,r,g,b),
+         the stride distance between one group to the next is 24 bytes */
+        gl.vertexAttribPointer(vertexAttr, 3, gl.FLOAT, false, 24, 0); /* (x,y,z) begins at offset 0 */
+        gl.vertexAttribPointer(colorAttr, 3, gl.FLOAT, false, 24, 12); /* (r,g,b) begins at offset 12 */
+
+        for (let k = 0; k < this.indices.length; k++) {
+            let obj = this.indices[k];
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, obj.buffer);
+            gl.drawElements(obj.primitive, obj.numPoints, gl.UNSIGNED_BYTE, 0);
+        }
+    }
+}
