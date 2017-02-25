693e144c5cfdb1937ce72ecf71ada2397e7d2a74
christopherfracassi <fracassc@mail.gvsu.edu>
2017-02-25 18:34:08 -0500

    shaders

--- fshader.glsl
+++ fshader.glsl
@@ -2,5 +2,8 @@ precision mediump float;
 
 varying vec4 varColor;
 void main() {
-    gl_FragColor = varColor;
+    if(gl_FrontFacing)
+        gl_FragColor = varColor;
+    else
+        gl_FragColor = vec4(1,0,0,1);
 }
