9c8bf39ec5c8c5b8a5baef61ddd6e955e509227f
Charles Billingsley <charlesebillingsley@yahoo.com>
2017-02-25 19:30:24 -0500

    added structure for viewing the camera

--- Camera.js
+++ Camera.js
@@ -5,15 +5,15 @@
 class Camera {
     constructor(gl){
         this.body = new Cube(gl, 0.4, 4);
-        this.lens = new Ring(gl, 0.3, 0.2, 0.3, 10, 1);
-        this.glass = new Sphere(gl, 0.2, 15);
+        // this.lens = new Ring(gl, 0.3, 0.2, 0.3, 10, 1);
+        // this.glass = new Sphere(gl, 0.2, 15);
 
         this.tmp = mat4.create();
     }
 
     draw (vertexAttr, colorAttr, modelUniform, coordFrame) {
         this.body.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
-        this.lens.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
-        this.glass.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
+        // this.lens.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
+        // this.glass.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
     }
 }
