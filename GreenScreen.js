3f4193b23c39654de46ceb3f8a4f463e65552631
Charles Billingsley <charlesebillingsley@yahoo.com>
2017-03-13 14:48:33 -0400

    Added simple green screen
    Next steps:
    -Find out how to zoom out a bit
    -Technical write up

---
+++ GreenScreen.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Charles Billingsley
+ */
+
+class GreenScreen {
+    constructor(gl){
+        /** Colors */
+        // Screen
+        this.Color1 = vec3.fromValues(0, .8, .5);
+        this.Color2 = vec3.fromValues(0, 0.7, .3);
+
+        /** Objects */
+        // Screen
+        this.greenScreen = new Cube2(gl, 10, 1, this.Color1, this.Color2, this.Color2);
+
+        /** Transformations */
+        // Screen
+        this.screenTransform = mat4.create();
+        let stretchTop = vec3.fromValues(.01,.5,.25);
+        mat4.scale(this.screenTransform, this.screenTransform, stretchTop);
+
+        let moveScreenUp = vec3.fromValues(0, 0, .2);
+        mat4.translate (this.screenTransform, this.screenTransform, moveScreenUp);
+
+        this.blank = mat4.create();
+
+    }
+
+    draw (vertexAttr, colorAttr, modelUniform, coordFrame) {
+        // Screen
+        mat4.mul(this.blank, coordFrame, this.screenTransform);
+        this.greenScreen.draw(vertexAttr, colorAttr, modelUniform, this.blank);
+    }
+}
