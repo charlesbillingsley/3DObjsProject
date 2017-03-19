6988f8b1ea616e4f5c3fd88fead96128e888b7bc
Charles Billingsley <charlesebillingsley@yahoo.com>
2017-03-19 18:06:16 -0400

    Added Flash

--- Camera.js
+++ Camera.js
@@ -1,5 +1,7 @@
 /**
- * Created by christopherfracassi on 2/25/17.
+ * Created by:
+ * Charles Billingsley
+ * Chris Fracassi
  */
 class Camera {
     constructor(gl){
@@ -9,6 +11,16 @@ class Camera {
         this.bodyColor2 = vec3.fromValues(.35, .35, .55);
         this.bodyColor3 = vec3.fromValues(.33, .33, .53);
 
+        // Flash
+        this.flashColor1 = vec3.fromValues(.45, .45, .55);
+        this.flashColor2 = vec3.fromValues(.50, .50, .60);
+        this.flashColor3 = vec3.fromValues(.35, .35, .45);
+
+        // Flash Glass
+        this.flashGlassColor1 = vec3.fromValues(.8, .8, .8);
+        this.flashGlassColor2 = vec3.fromValues(.9, .9, .9);
+        this.flashGlassColor3 = vec3.fromValues(1, 1 , 1);
+
         // Camera Lens
         this.lensColor1 = vec3.fromValues(.4, .4, .7);
         this.lensColor2 = vec3.fromValues(.5, .5, .9);
@@ -17,7 +29,7 @@ class Camera {
         this.glassColor1 = vec3.fromValues(0, 1, 1);
         this.glassColor2 = vec3.fromValues(0, 0.9, .9);
 
-        // Top of the tripod
+        // Top of the tripodS
         this.triTopColor1 = vec3.fromValues(.35, .35, .65);
         this.triTopColor2 = vec3.fromValues(.35, .35, .55);
 
@@ -27,6 +39,8 @@ class Camera {
 
         /** Create the objects */
         this.body = new Cube2(gl, 0.3, 4, this.bodyColor1, this.bodyColor2, this.bodyColor3);
+        this.flash = new Cube2(gl, 0.1, 4, this.flashColor1, this.flashColor2, this.flashColor3);
+        this.flashGlass = new Cube2(gl, 0.1, 4, this.flashGlassColor1, this.flashGlassColor2, this.flashGlassColor3);
         this.lens = new Ring(gl, 0.13, 0.08, 0.2, 30, 1, this.lensColor1, this.lensColor2);
         this.glass = new Sphere(gl, 0.1, 15, this.glassColor1, this.glassColor2);
         this.tripodTop = new Sphere(gl, 0.07, 15, this.triTopColor1, this.triTopColor2);
@@ -37,8 +51,25 @@ class Camera {
         /** Create the transforms */
         // Camera Body
         this.bodyTransform = mat4.create();
-        let stretchBody = vec3.fromValues(2,1,1);
+        let stretchBody = vec3.fromValues(2, 1, 1);
         mat4.scale(this.bodyTransform, this.bodyTransform, stretchBody);
+
+        // Flash
+        this.flashTransform = mat4.create();
+        let stretchFlash = vec3.fromValues(2, 1, 1);
+        mat4.scale(this.flashTransform, this.flashTransform, stretchFlash);
+
+        let moveFlash = vec3.fromValues(.05, .05, .2);
+        mat4.translate (this.flashTransform, this.flashTransform, moveFlash);
+
+        // Flash Glass
+        this.flashGlassTransform = mat4.create();
+
+        let stretchFlashGlass = vec3.fromValues(1.8, .8, .8);
+        mat4.scale(this.flashGlassTransform, this.flashGlassTransform, stretchFlashGlass);
+
+        let moveFlashGlass = vec3.fromValues(.055, .08, .25);
+        mat4.translate (this.flashGlassTransform, this.flashGlassTransform, moveFlashGlass);
 
         // Lens
         this.lensTransform = mat4.create();
@@ -97,6 +128,14 @@ class Camera {
         // Top of the tripod
         mat4.mul(this.blank, coordFrame, this.triTopTransform);
         this.tripodTop.draw(vertexAttr, colorAttr, modelUniform, this.blank);
+
+        // Flash
+        mat4.mul(this.blank, coordFrame, this.flashTransform);
+        this.flash.draw(vertexAttr, colorAttr, modelUniform, this.blank);
+
+        // Flash Glass
+        mat4.mul(this.blank, coordFrame, this.flashGlassTransform);
+        this.flashGlass.draw(vertexAttr, colorAttr, modelUniform, this.blank);
 
         // Camera Body
         mat4.mul(this.blank, coordFrame, this.bodyTransform);
